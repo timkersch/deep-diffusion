db6afbb2a0ce5aafa0164cfa2e74014e357b31cd
Tim Kerschbaumer <maq@Tims-MacBook-Air.local>
2017-07-02 17:50:12 +0200

    Updates

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ import nibabel as nib
 from matplotlib.pyplot import hist
 import matplotlib.pyplot as plt
 
-file1 = './data/10000voxels_uniform_p=0_rad=1E-6_sep=2.1E-6_HPC-scheme.bfloat'
-file2 = './data/10000voxels_uniform_p=0_rad=1E-6_sep=2.1E-6_HPC-scheme2_seed=843276243.bfloat'
 hpc = './data/dwi.Bfloat'
 
 fileList = ['./data/1000voxels_uniform_p=0_rad=0.1E-6_sep=1.1E-6_HPC-scheme.bfloat',
@@ -23,45 +21,31 @@ targetList = [(0.1E-6, 1.1E-6),
 			  (2E-6, 4.1E-6)]
 
 
-def read_float(filename):
-	f = open(filename, "r")
-	arr = np.fromfile(f, dtype='>f4')
-	return arr
-
-
-def read_floats(filenames):
-	files = []
-	for file in range(0, len(filenames)):
-		files.append(to_voxels(read_float(filenames[file])))
-	return files
-
-
-def read_ni(filename):
-	arr = nib.load(filename)
-	return arr
-
-
-def to_voxels(arr, channels=288, skip_ones=True):
-	# N = no samples = 1000
-	# C = channels = 288
-	# W = width = 1
-	# H = height = 1
-	# D = depth = 1
-	no_samples = int(arr.size / channels)
-	if skip_ones:
-		return np.reshape(arr, (no_samples, channels))
-	return np.reshape(arr, (no_samples, 1, 1, 1, channels))
+def get_data(split_ratio=0.7):
+	X, y = _load_data()
+	split = int(X.shape[0] * split_ratio)
+	indices = np.random.permutation(X.shape[0])
+	training_idx, test_idx = indices[:split], indices[split:]
+	return X[training_idx, :], y[training_idx, :], X[test_idx, :], y[test_idx, :]
 
 
 def get_pred_data(sample_size=None):
-	arr = to_voxels(read_float(hpc))
+	arr = _to_voxels(_read_float(hpc))
 	if sample_size is not None:
 		np.random.shuffle(arr)
 		return arr[0:sample_size, :]
 	return arr
 
 
-def load_data():
+def plot_features():
+	X, y = _load_data()
+	for i in range(0, X.shape[1]):
+		x = X[:,i]
+		n, bins, patches = hist(x, bins='auto', range=None, normed=False, weights=None, cumulative=False, bottom=None)
+		plt.show()
+
+
+def _load_data():
 	X = np.empty((len(fileList) * 1000, 288))
 	y = np.empty((len(fileList) * 1000, 2))
 
@@ -70,7 +54,7 @@ def load_data():
 	for i in xrange(0, len(fileList)):
 		file = fileList[i]
 		targetTuple = targetList[i]
-		vals = to_voxels(read_float(file), skip_ones=True)
+		vals = _to_voxels(_read_float(file), skip_ones=True)
 
 		X[start:end] = vals
 		y[start:end] = np.array(targetTuple)
@@ -81,17 +65,24 @@ def load_data():
 	return X, y
 
 
-def plot_features():
-	X, y = load_data()
-	for i in range(0, X.shape[1]):
-		x = X[:,i]
-		n, bins, patches = hist(x, bins='auto', range=None, normed=False, weights=None, cumulative=False, bottom=None)
-		plt.show()
+def _read_float(filename):
+	f = open(filename, "r")
+	arr = np.fromfile(f, dtype='>f4')
+	return arr
 
 
-def get_data(split_ratio=0.7):
-	X, y = load_data()
-	split = int(X.shape[0] * split_ratio)
-	indices = np.random.permutation(X.shape[0])
-	training_idx, test_idx = indices[:split], indices[split:]
-	return X[training_idx, :], y[training_idx, :], X[test_idx, :], y[test_idx, :]
+def _read_ni(filename):
+	arr = nib.load(filename)
+	return arr
+
+
+def _to_voxels(arr, channels=288, skip_ones=True):
+	# N = no samples = 1000
+	# C = channels = 288
+	# W = width = 1
+	# H = height = 1
+	# D = depth = 1
+	no_samples = int(arr.size / channels)
+	if skip_ones:
+		return np.reshape(arr, (no_samples, channels))
+	return np.reshape(arr, (no_samples, 1, 1, 1, channels))
