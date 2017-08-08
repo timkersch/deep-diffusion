438b48cd8d246a327e7cf3c36189775ed4ff88ff
Tim Kerschbaumer <maq@Tims-MacBook-Air.local>
2017-08-08 22:13:52 +0200

    Updates

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import dataset
 from networks.voxel_network import VoxNet
 import theano.tensor as T
 import json
 import os
 import errno
-import matplotlib.pyplot as plt
 from utils import rmsd, print_and_append
 
 
 def train(model_id, train_set, validation_set, config, super_dir='models/', show_plot=False):
-	# Prepare Theano variables for inputs and targets
 	input_var = T.dmatrix('inputs')
 	target_var = T.dmatrix('targets')
 
@@ -62,9 +63,8 @@ def parameter_search(dir='models/search/'):
 		config = json.load(data_file)
 	train_set, validation_set, test_set = dataset.load_dataset(config['no_dwis'], split_ratio=(0.7, 0.2, 0.1))
 
-	learning_rates = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
-	batch_sizes = [32, 54, 128, 256]
-	batch_norms = [False, True]
+	learning_rates = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
+	batch_sizes = [32, 64, 128, 256]
 	scale_outputs = [True, False]
 	early_stoppings = [5, 10]
 
@@ -72,33 +72,31 @@ def parameter_search(dir='models/search/'):
 	best_index = -1
 
 	index = 1
-	no_configs = len(learning_rates)*len(batch_sizes)*len(batch_norms)*len(scale_outputs)*len(early_stoppings)
+	no_configs = len(learning_rates)*len(batch_sizes)*len(scale_outputs)*len(early_stoppings)
 	print "Beginning grid search with {} configurations".format(no_configs)
-	for batch_norm in batch_norms:
-		for scale_output in scale_outputs:
-			for early_stopping in early_stoppings:
-				for batch_size in batch_sizes:
-					for learning_rate in learning_rates:
-						print "Fitting model {} of {} with l-rate: {} batch-size: {} e-stopping: {} scale-out: {} batch-norm: {}".format(index, no_configs, learning_rate, batch_size, early_stopping, scale_output, batch_norm)
-						config['optimizer']['learning_rate'] = learning_rate
-						config['batch_size'] = batch_size
-						config['early_stopping'] = early_stopping
-						config['scale_outputs'] = scale_output
-						config['batch_norm'] = batch_norm
+	for scale_output in scale_outputs:
+		for early_stopping in early_stoppings:
+			for batch_size in batch_sizes:
+				for learning_rate in learning_rates:
+					print "Fitting model {} of {} with l-rate: {} batch-size: {} e-stopping: {} scale-out: {}".format(index, no_configs, learning_rate, batch_size, early_stopping, scale_output)
+					config['optimizer']['learning_rate'] = learning_rate
+					config['batch_size'] = batch_size
+					config['early_stopping'] = early_stopping
+					config['scale_outputs'] = scale_output
 
-						model = train(super_dir=dir, train_set=train_set, validation_set=validation_set, model_id=index, config=config, show_plot=False)
+					model = train(super_dir=dir, train_set=train_set, validation_set=validation_set, model_id=index, config=config, show_plot=False)
 
-						test_pred = model.predict(test_set[0])
-						rms_distance = rmsd(test_pred, test_set[1])
-						print 'Test RMSE: {} \n'.format(rms_distance)
+					test_pred = model.predict(test_set[0])
+					rms_distance = rmsd(test_pred, test_set[1])
+					print 'Test RMSE: {} \n'.format(rms_distance)
 
-						if rms_distance < lowest_rmsd:
-							lowest_rmsd = rms_distance
-							best_index = index
+					if rms_distance < lowest_rmsd:
+						lowest_rmsd = rms_distance
+						best_index = index
 
-						print 'Current best model is: {} with test RMSE: {} \n'.format(best_index, lowest_rmsd)
+					print 'Current best model is: {} with test RMSE: {} \n'.format(best_index, lowest_rmsd)
 
-						index += 1
+					index += 1
 
 	print "Done... Best was model with index {} and test RMSE {}".format(best_index, lowest_rmsd)
 
